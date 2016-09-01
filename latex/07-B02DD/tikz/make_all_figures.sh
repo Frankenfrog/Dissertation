dbee49f059f90911a1355ef50f35aecdbb8ad9ff
Frank Meier <frank.meier@tu-dortmund.de>
2016-09-01 15:02:17 +0200

    included systematic studies on fit model and other sources

--- latex/07-B02DD/tikz/make_all_figures.sh
+++ latex/07-B02DD/tikz/make_all_figures.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-figs=(obsMass_summed_pull obsTime_summed_pull_logy Acceptancespline_nolog obsTimeErr_True_pull_logy Acceptancespline_nolog_MC_KKpi Acceptancespline_nolog_MC_Kpipi)
+figs=(obsMass_summed_pull obsTime_summed_pull_logy Acceptancespline_nolog
+obsTimeErr_True_pull_logy Acceptancespline_nolog_MC_KKpi
+Acceptancespline_nolog_MC_Kpipi FitResultsCorrMatrix_RedBlueDiscrete_wText
+Asymmetry Likelihoodscan_C Likelihoodscan_sin2b parSigTimeSin2b_pull_fitbias parSigTimeC_pull_fitbias Profile_DecayTime_SS)
 
 for fig in ${figs[@]}; do
   pdflatex --jobname=pdf/$fig all_figures.tex
