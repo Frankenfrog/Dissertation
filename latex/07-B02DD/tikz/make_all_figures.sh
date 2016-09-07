3d32bedc6129cf130d90140a63f076e6c7f6e697
Frank Meier <frank.meier@tu-dortmund.de>
2016-09-07 13:06:17 +0200

    eps and png not needed for now

--- latex/07-B02DD/tikz/make_all_figures.sh
+++ latex/07-B02DD/tikz/make_all_figures.sh
@@ -7,9 +7,9 @@ Asymmetry Likelihoodscan_C Likelihoodscan_sin2b parSigTimeSin2b_pull_fitbias par
 
 for fig in ${figs[@]}; do
   pdflatex --jobname=pdf/$fig all_figures.tex
-  pdf2ps pdf/$fig.pdf $fig.ps
-  ps2eps $fig.ps
-  mv $fig.eps eps/$fig.eps
-  rm $fig.ps
-  convert -density 300 -flatten pdf/$fig.pdf png/$fig.png
+  # pdf2ps pdf/$fig.pdf $fig.ps
+  # ps2eps $fig.ps
+  # mv $fig.eps eps/$fig.eps
+  # rm $fig.ps
+  # convert -density 300 -flatten pdf/$fig.pdf png/$fig.png
 done
