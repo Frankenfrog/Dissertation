4b5c33dae8a0d51363e63957e94e540ff345d7b2
Frank Meier <frank.meier@tu-dortmund.de>
2016-09-30 18:46:26 +0200

    finalised Bd2JpsiKS selection and decay time acceptance

---
+++ latex/06-Bd2JpsiKS/tikz/make_all_figures.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+figs=(Acceptancespline_zoomed_hlt1 Acceptancespline_zoomed_hlt2)
+
+for fig in ${figs[@]}; do
+  pdflatex --jobname=pdf/$fig all_figures.tex
+  # pdf2ps pdf/$fig.pdf $fig.ps
+  # ps2eps $fig.ps
+  # mv $fig.eps eps/$fig.eps
+  # rm $fig.ps
+  # convert -density 300 -flatten pdf/$fig.pdf png/$fig.png
+done
